072c78b1a476fef932d9dfbd403c85201c58be19
liush <liushaohua@xiucai.com>
2019-08-23 14:21:26 +0800

    ignore

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  var materialApp = new MaterialApp(
+    title: 'test',
+    home: new Text('aaaaabcc'),
+  );
+  return runApp(materialApp);
+}
